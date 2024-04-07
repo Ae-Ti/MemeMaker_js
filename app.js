1e8faf15d6736a531b42f2200678be1ee8cbfcc4
Ae-Ti <taeilbae7@gmail.com>
2024-04-07 22:58:04 +0900

    javascript picture app v.2 - April 7

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
+
+ctx.fillRect(200,200,50,200);
+ctx.fillRect(400,200,50,200);
+ctx.lineWidth = 2;
+ctx.strokeRect(300,300,50,100);
+ctx.fillRect(200,200,200, 20);
+ctx.moveTo(200,200);
+ctx.lineTo(325,100);
+ctx.lineTo(450,200);
 ctx.fill();
 
-ctx.beginPath();
-ctx.rect(350, 350, 100, 100);
-ctx.fillStyle = 'Red';
-setTimeout(()=>{ctx.fill();},5000);
+
+ctx.fillRect(500,500,5,5)
+ctx.beginPath()
+ctx.arc(500,50,20,0,2*Math.PI);
+ctx.fill();
+ctx.beginPath()
+ctx.fillStyle='red';
+ctx.arc(510,55,5,0,2*Math.PI);
+ctx.fill();
+ctx.beginPath()
+ctx.fillStyle='white';
+ctx.arc(495,55,5,1*Math.PI,2*Math.PI);
+ctx.fill();
