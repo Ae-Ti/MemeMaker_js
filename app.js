8e96398d8cc3fe57be60b7b992c3686ea457adbd
Ae-Ti <taeilbae7@gmail.com>
2024-04-07 23:30:20 +0900

    javascript picture app v.1.3

--- app.js
+++ app.js
@@ -2,27 +2,19 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
-ctx.fillRect(200,200,50,200);
-ctx.fillRect(400,200,50,200);
-ctx.lineWidth = 2;
-ctx.strokeRect(300,300,50,100);
-ctx.fillRect(200,200,200, 20);
-ctx.moveTo(200,200);
-ctx.lineTo(325,100);
-ctx.lineTo(450,200);
-ctx.fill();
+ctx.lineWidth=2;
+const colors = [
+    "#00a8ff","#9c88ff","#fbc531","#4cd137","#e84118","#273c75","#353b48",
+    "#7f8fa6","#f5f6fa"
+]
 
 
-ctx.fillRect(500,500,5,5)
-ctx.beginPath()
-ctx.arc(500,50,20,0,2*Math.PI);
-ctx.fill();
-ctx.beginPath()
-ctx.fillStyle='red';
-ctx.arc(510,55,5,0,2*Math.PI);
-ctx.fill();
-ctx.beginPath()
-ctx.fillStyle='white';
-ctx.arc(495,55,5,1*Math.PI,2*Math.PI);
-ctx.fill();
+function onClick(event){
+    ctx.moveTo(0,0);
+    const color = colors[Math.floor(Math.random()*colors.length)];
+    ctx.strokeStyle = color;
+    ctx.lineTo(event.offsetX,event.offsetY);
+    ctx.stroke();
+    ctx.beginPath();
+}
+canvas.addEventListener("mousemove",onClick);
