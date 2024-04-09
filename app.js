c0b185b8dadd1546462de7d1cfdae9d847d50706
Ae-Ti <taeilbae7@gmail.com>
2024-04-09 15:34:02 +0900

    javascript picture app v.3

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const saveBtn = document.getElementById("save");
+const fileInput = document.getElementById("file");
+const textInput = document.getElementById("text");
+
 const eraserBtn = document.getElementById("eraser-btn")
 const destroyBtn = document.getElementById("destroy-btn");
 const modeBtn = document.getElementById("mode-btn");
@@ -17,7 +21,7 @@ canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
 ctx.lineWidth= lineWidth.value;
-
+ctx.lineCap = "round";
 let isPainting = false;
 let isFilling = false;
 
@@ -77,6 +81,34 @@ function onEraserClick(){
     modeBtn.innerText = "fill";
 }
 
+function onFileChange(event){
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url;
+    image.onload = function(){
+        ctx.drawImage(image,0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+        fileInput.value = null;
+    }
+}
+function onDoubleClick(event){
+    const text = textInput.value;
+    if(text!==""){
+        ctx.save();   
+        ctx.lineWidth = 1;
+        ctx.font = "48px gothic";
+        ctx.fillText(text, event.offsetX, event.offsetY);
+        ctx.restore();
+    }
+}
+function onSaveClick(){
+    const url = canvas.toDataURL();
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "myDrawing.png";
+    a.click();
+}
+
 canvas.addEventListener("mousemove",onMove);
 canvas.addEventListener("mousedown",startPainting);
 canvas.addEventListener("mouseup",cancelPainting);
@@ -91,3 +123,7 @@ colorOptions.forEach(color => color.addEventListener("click",onColorClick))
 modeBtn.addEventListener("click",onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click",onEraserClick);
+
+fileInput.addEventListener("change",onFileChange);
+canvas.addEventListener("dblclick",onDoubleClick);
+saveBtn.addEventListener("click", onSaveClick);
